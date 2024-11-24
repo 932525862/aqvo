7eece3b1755f7b4f2587381ee7a214b3571b1910
asrorovuz <asrorovsardor07@gmail.com>
2024-11-24 20:53:33 +0500

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-red-800 pb-10 pt-20">
+      <div className="container">
+        <div className="flex flex-col items-center justify-center gap-y-5 text-white">
+          <p className="max-w-[564px] text-center">
+            Siz izlagan ta'm, musaffolik, poklik va o‘zgachalik... Barchasi bir
+            nuqtada jamlanib, “Evita”'ga aylanadi!
+          </p>
+          <div></div>
+          <p>© Barcha huquqlar himoyalangan</p>
+          <p className="text-white/80">Made by marketing agency Limsa</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ import leftArrowIcon from "../assets/left-arrow.svg";
 import aboutImg1 from "../assets/about1.jpg";
 import aboutImg2 from "../assets/about2.jpg";
 import aboutImg3 from "../assets/about.jpg";
+import sertificatImg from "../assets/sertificat.jpg";
 
 const About = () => {
   return (
@@ -27,7 +28,7 @@ const About = () => {
           </h2>
         </div>
 
-        <div className="flex gap-x-10 mb-20">
+        <div className="flex gap-x-10 mb-16">
           <div className="w-[46%] rounded-xl overflow-hidden">
             <img src={aboutImg1} alt="taqvo image" className="h-full w-full object-cover"/>
           </div>
@@ -52,7 +53,7 @@ const About = () => {
           </div>
         </div>
 
-        <div className="flex flex-row-reverse gap-x-10 mb-20">
+        <div className="flex flex-row-reverse gap-x-10 mb-16">
           <div className="w-[46%] rounded-xl overflow-hidden">
             <img src={aboutImg2} alt="taqvo image" className="h-full w-full object-cover"/>
           </div>
@@ -77,17 +78,12 @@ const About = () => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-y-5">
+        <div className="flex flex-col gap-y-5 mb-16">
           <div className="w-full rounded-xl overflow-hidden">
             <img src={aboutImg3} alt="taqvo image" className="h-full w-full object-cover"/>
           </div>
 
           <div className="flex flex-col gap-y-5 justify-between">
-            <h4 className="text-[24px] text-white font-medium">
-              <span className="text-color1 block">WATER LIFE MINERAL</span>{" "}
-              O‘zbekistondagi eng yirik ichimlik suvi ishlab chiqaruvchi
-              kompaniyalardan biri hisoblanadi.
-            </h4>
             <p className="text-[15px] font-medium text-white">
               Biz mijozlarimizga barcha standartlar va talablarga javob
               beradigan yuqori sifatli ichimlik suvini taklif qilishga
@@ -99,6 +95,17 @@ const About = () => {
               ekanligini ta'minlash uchun qattiq tekshiruv a nazoratning ko'p
               bosqichlaridan o'tadi.
             </p>
+          </div>
+        </div>
+
+        <div>
+          <h3 className="oswald text-color1 text-3xl font-medium mb-5">BIZNING SERTIFIKATLARIMIZ</h3>
+
+          <div className="grid grid-cols-4 gap-x-5">
+            <img src={sertificatImg} alt="taqvo sertificat image" />
+            <img src={sertificatImg} alt="taqvo sertificat image" />
+            <img src={sertificatImg} alt="taqvo sertificat image" />
+            <img src={sertificatImg} alt="taqvo sertificat image" />
           </div>
         </div>
       </div>
